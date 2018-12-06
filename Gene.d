1be9016cc3cf781fbe894a54b53f55fbeda95d29
Zac00195 <45643334+Zac00195@users.noreply.github.com>
2018-12-05 18:00:16 -0600

    Add files via upload

---
+++ Gene.d
@@ -0,0 +1,7 @@
+Gene.o: Gene.cpp Gene.h Allele.h Chromosome.h
+
+Gene.h:
+
+Allele.h:
+
+Chromosome.h:
---
+++ GeneSequencer.d
@@ -0,0 +1,10 @@
+GeneSequencer.o: GeneSequencer.cpp Allele.h Gene.h Chromosome.h \
+ GeneSequencer.h
+
+Allele.h:
+
+Gene.h:
+
+Chromosome.h:
+
+GeneSequencer.h:
